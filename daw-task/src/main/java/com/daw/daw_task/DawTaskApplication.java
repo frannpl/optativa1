29de8b03feac6c3dee8332f2f978525d3a041db6
Fran López <franlopezpineda@gmail.com>
2025-09-23 09:15:57 +0200

    comit1

---
+++ daw-task/src/main/java/com/daw/daw_task/DawTaskApplication.java
@@ -0,0 +1,13 @@
+package com.daw.daw_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawTaskApplication.class, args);
+	}
+
+}
---
+++ daw-task/src/test/java/com/daw/daw_task/DawTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.daw_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
